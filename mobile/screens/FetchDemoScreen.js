d6984fe90b974fe18b5c31fc1f75f38305b1a486
Max Harper <maxharp3r@gmail.com>
2017-11-21 14:53:29 -0600

    update fetch demo to demonstrate authenticated api call

--- mobile/screens/FetchDemoScreen.js
+++ mobile/screens/FetchDemoScreen.js
@@ -13,11 +13,15 @@ class FetchDemoScreen extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      message: 'no message yet'
+      message: 'no message yet',
+      authStatus: 'not tested yet',
     }
   }
 
   componentDidMount() {
+    const {getAuthorizationHeader} = this.props.screenProps;
+
+    // unauthenticated demo
 
     fetch(`${config.API_BASE}/hello-world`)
       .then((response) => response.json())
@@ -28,6 +32,31 @@ class FetchDemoScreen extends React.Component {
       .catch((error) => {
         console.error(error);
       });
+
+    // authenticated demo
+
+    let myRequest = new Request(`${config.API_BASE}/api/db/protected`, {
+      method: 'GET',
+      // this header sends the user token from auth0
+      headers: getAuthorizationHeader()
+    });
+    // console.log('request', myRequest);
+
+    fetch(myRequest)
+      .then(response => {
+        // https://www.tjvantoll.com/2015/09/13/fetch-and-errors/
+        if (!response.ok) {
+          throw Error(response.statusText);
+        }
+        return response;
+      })
+      .then(res => res.json())
+      .then(json => {
+        this.setState({ 'authStatus': 'works!' });
+      })
+      .catch(error => {
+        this.setState({ 'authStatus': 'broken; are you logged in? check logs.' });
+      });
   }
 
   render() {
@@ -35,6 +64,7 @@ class FetchDemoScreen extends React.Component {
       <View>
         <Text>fetch demo</Text>
         <Text>message: {this.state.message}</Text>
+        <Text>auth: {this.state.authStatus}</Text>
       </View>
     );
   }
